e6d2206cce49bfb40fb20e5d0f4187725bec301c
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-03-25 00:16:48 +0100

    Update RailFence.java

--- ds/RailFence.java
+++ ds/RailFence.java
@@ -5,3 +5,24 @@ public class RailFence {
 		       System.out.println(encoding(args[1],Integer.parseInt(args[2])));
        
 	}
+	public static String encoding(String plaintext, int key) {
+	  String ciphertext = "" ;
+	  boolean kontrollo=false;
+	  int j=0;
+	  int rreshta=key;
+	  int kolona=plaintext.length();
+	  
+	  char[][] a= new char [rreshta][kolona];
+	  
+	  for(int i=0; i< kolona; i++) {
+		  if(j ==0 || j== key-1)    
+			  kontrollo = !kontrollo;
+		  
+		  a[j][i]= plaintext.charAt(i);
+		  
+		  
+		 if(kontrollo)
+			 j++;
+		 else
+			 j=0;
+	  }
