8199106a9ea8bc7e046436ac9727ee4c24a96f88
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-03-25 00:06:58 +0100

    Update Caesar.java

--- ds/Caesar.java
+++ ds/Caesar.java
@@ -5,4 +5,12 @@ public static String encoding (String plaintext, int key)
     {
         plaintext = plaintext.toLowerCase() ;
         String ciphertext = "" ;
-       
+        for (int i = 0 ; i < plaintext.length() ; i++)
+        {
+            int pozita = alfabeti.indexOf(plaintext.charAt(i)) ;             
+            int enCh = (key + pozita) % 26 ;
+            char ch = alfabeti.charAt(enCh) ;  
+            ciphertext += ch ; 
+        }
+        return ciphertext ;
+    }
