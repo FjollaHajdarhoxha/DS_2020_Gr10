f5bcf3d1b3c2e86b8c7565d74fbd328c0a9c4add
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-03-25 18:00:11 +0100

    Create Case.java

---
+++ ds/Case.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class test1 {
+
+	   static String upper(String str)
+	   {
+		   String strUpper=str.toUpperCase();
+		   return strUpper;
+	   }
+	   static String lower(String str)
+	   {
+		   String strLower=str.toLowerCase();
+		   return strLower;
+	   }
+	
+	   static void inverse (StringBuffer str)
+	   {
+		   int ln=str.length();
+		   for(int i=0; i<ln; i++)
+		   {
+			   Character c= str.charAt(i);
+			   if(Character.isLowerCase(c))
+				   str.replace(i,i+1,Character.toUpperCase(c)+" ");
+			   else 
+				   str.replace(i,i+1,Character.toLowerCase(c)+" ");
+			
+			 }
+	   }
+	
+	   static String capitalize (String str)
+	   {
+		     char ch[]=str.toCharArray();
+		     for(int i=0; i<str.length();i++)
+		     {
+		    	if(i==0 && ch[i] != ' ' || ch[i] !=' ' && ch[i-1]==' '	)
+		    	{
+		    		if(ch[i]>= 'a'&& ch[i]<='z')
+		    		
+		    			{ch[i]=(char)(ch[i]-'a'+'A');}
+		    	}
+		    	else if (ch[i]>='A' && ch[i]<='Z')
+		    	    ch[i]=(char)(ch[i]+'a'-'A');
+		     }
+		     String st=new String(ch);
+		     return st;
+		   }
+	    
+	   
+	   
+	public static void main(String[] args)
+	{ 
+		
+		Scanner input= new Scanner(System.in);
+		System.out.print("Shkruani fjaline e deshiruar: ");
+		String str=input.nextLine();
+		
+		System.out.println(upper(str));
+		System.out.println(lower(str));
+		System.out.println(capitalize(str));
+		
+		
+		input.close();
+
+	}
+
+}
