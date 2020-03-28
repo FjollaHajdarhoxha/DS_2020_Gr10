9e793bd6505211f5d946c44788b2224b0910cb11
rinalutolli <52406042+rinalutolli@users.noreply.github.com>
2020-03-29 00:47:37 +0100

    Add files via upload

--- ds/RailFence.java
+++ ds/RailFence.java
@@ -2,9 +2,10 @@
 public class RailFence {
 
 	public static void main(String[] args) {
-		if("encrypt".equals(args[0]))
+		 if("encrypt".equals(args[0]))
 		       System.out.println(encoding(args[1],Integer.parseInt(args[2])));
-              if("dencrypt".equals(args[0]))System.out.println(decoding(args[1],Integer.parseInt(args[2])));
+		    if("decrypt".equals(args[0]))
+		        System.out.println(decoding(args[1],Integer.parseInt(args[2])));
        
 	}
    public static String encoding(String plaintext, int key) {
@@ -36,68 +37,62 @@ public class RailFence {
 		
 		  }
 		  }
-
+	  for(int i=0; i<rreshta;i++) {
+		  for(int k=0;k<kolona;k++) {
+		  System.out.print(a[i][k]+"");
+		 
+		  }
+		  
+	 System.out.println();
+   }
       return ciphertext;
   }
-
-
-
    public static String decoding (String ciphertext, int key) {
 	   String deciphertext = "" ;
           boolean kontrollo=false;
+          int rreshta=key;
           int j=0;
-	  int kolona=plaintext.length();
+	  int kolona=ciphertext.length();
 	  
 	  char[][] a= new char [rreshta][kolona];
 
  
-          for(int i=0;i<rreshta;i++){
-                     if(j==0||j==kolona-1)
-                      kontrollo=!kontrollo;
-  
-  
-  
-                          a[j][i]='*';
-                         if(kontrollo)j++;
-                           else j=0;
- 
- }
- int index=0;
- 
- for(int i=0;i<rreshta;i++){
-  for(int k=0;k<kolona;k++){
-   
-   
-   if(a[i][k]=='*'&&index<plaintext.length()){
-   a[i][k]=plaintext.charAt(index++);
-   
-   }
-   
-  }
-  
- }
- 
- kontrollo=false;
- j=0;
- 
- for(int i=0;i<col;i++){
-  if( j==0||j==row-1)
-   kontrollo=!kontrollo;
-  
-  
-  deciphertext+=a[j][i];
-  
- 
-  if(kontrollo)j++;
-  else j=0;
- 
- }
- 
- 
+	  for(int i=0; i< kolona; i++) {
+		  if(j ==0 || j== key-1)    
+			  kontrollo = !kontrollo;
+		  
+		   a[j][i]='*';
+		  
+		  
+		 if(kontrollo)
+			 j++;
+		 else
+			 j=0;
+	  }
 
-         
-          
-	   
+	  int pozita=0;
+	  kontrollo=false;
+	  for(int i=0; i<rreshta;i++) {
+		  for(int k=0;k<kolona;k++) {
+			  if(a[i][k]=='*' && pozita < kolona)						  
+				  a[i][k]=ciphertext.charAt(pozita++);
+		
+		  }
+		  }
+	
+	  j=0;
+	  for(int i=0; i< kolona; i++) {
+		  if(j ==0 || j== key-1)    
+			  kontrollo = !kontrollo;
+		  
+		  deciphertext += a[j][i];
+		  
+		  
+		 if(kontrollo)
+			 j++;
+		 else
+			 j=0;
+	  }
 	   return deciphertext ;
 }
 }
