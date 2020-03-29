f34ceea3d9b30497e5b7cffa006d53662f3afd92
rinalutolli <52406042+rinalutolli@users.noreply.github.com>
2020-03-29 20:32:15 +0200

    Add files via upload

--- ds/RailFence.java
+++ ds/RailFence.java
@@ -2,16 +2,11 @@
 public class RailFence {
 
 	public static void main(String[] args) {
-		if(args.length == 0)
-	     	System.out.println("Ju lutem jep njerin nga argumentet:"+"\n"+" encrypt" +"\n"
-	                           +" decrypt" +"\n"+
-	      "Shtyp 1 per te ndalur programin");
-	    if("1".equals(args[0]))
-	    	System.exit(1);
 		 if("encrypt".equals(args[0]))
 		       System.out.println(encoding(args[1],Integer.parseInt(args[2])));
 		    if("decrypt".equals(args[0]))
 		        System.out.println(decoding(args[1],Integer.parseInt(args[2])));
+		   
        
 	}
    public static String encoding(String plaintext, int key) {
@@ -19,7 +14,16 @@ public class RailFence {
 	  boolean kontrollo=false;
 	  int j=0;
 	  int rreshta=key;
+	  plaintext = plaintext.replaceAll("\\s+", "");
+	  
+	  
+	  do {
+		     plaintext=plaintext+"w";
+		     
+		} while (plaintext.length()%key!=0);
 	  int kolona=plaintext.length();
+	
+	 
 	  
 	  char[][] a= new char [rreshta][kolona];
 	  
@@ -36,14 +40,27 @@ public class RailFence {
 			 j=0;
 	  }
 	  
+	  
 	  for(int i=0; i<rreshta;i++) {
 		  for(int k=0;k<kolona;k++) {
-			  if(a[i][k]!=0)						  
+			  if(a[i][k]!=0)
+				  
 				  ciphertext += a[i][k];
-	
+		
 		  }
 		  }
-	
+	  System.out.println("Teksti i organizuar ne shirita:");
+	  for(int i=0; i<rreshta;i++) {
+		  for(int k=0;k<kolona;k++) {
+		  System.out.print(a[i][k]+"");
+		 
+		  }
+		  
+		 
+		  
+	 System.out.println();
+   }
+	  System.out.println("Teksti i enkriptuar:");
       return ciphertext;
   }
    public static String decoding (String ciphertext, int key) {
@@ -75,11 +92,13 @@ public class RailFence {
 		  for(int k=0;k<kolona;k++) {
 			  if(a[i][k]=='*' && pozita < kolona)						  
 				  a[i][k]=ciphertext.charAt(pozita++);
+			  
 		
 		  }
 		  }
 	
 	  j=0;
+	  System.out.println("Teksti i dekriptuar:");
 	  for(int i=0; i< kolona; i++) {
 		  if(j ==0 || j== key-1)    
 			  kontrollo = !kontrollo;
@@ -92,6 +111,7 @@ public class RailFence {
 		 else
 			 j=0;
 	  }
+	  
 	   return deciphertext ;
 }
 }
