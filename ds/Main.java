59097a58c5fd8c401f284c9c6e7d64a14536fd5b
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-04-26 14:21:54 +0200

    Add files via upload
    Shtese e nje programi Main ku thirren te gjitha funksionet tjera.

---
+++ ds/Main.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import javax.sound.sampled.*;
+import java.util.List;
+import java.io.*;
+public class Main  {
+
+	
+	public static void main(String[] args)throws  ArrayIndexOutOfBoundsException, IOException, LineUnavailableException, InterruptedException {
+ 
+	   
+	   Caesar C= new Caesar();
+	   RailFence R= new RailFence();
+	   Case Ca= new Case();	    
+	    if(args.length == 0 || args.length > 4 || args.length < 3){
+	    	
+	    	System.out.println("\n\t\tProgrami pranon deri në 4 argumente!\n");
+	    	 System.out.println("\t\t\tPërdorimi i Programit\n\n1.Për Kodin Caesar shtyp -> Caesar encrypt | decrypt <teksti> <celësi>\n" + "~Për BruteForce shtyp -> Caesar bruteforce <teksti>\n"+ "2.Për Kodin RailFence shtyp -> RailFence encrypt | decrypt <teksti> <celësi>\n" + "3.Për Kodin Case shtyp -> Case lower | upper | inverse | alternating | capitalize <teksti>");
+				System.exit(1);
+			}
+	    
+	    String text = args[2];
+	    String key=args[3];
+	    int k=Integer.parseInt(key);
+	    
+	    if("Caesar".equals(args[0])){
+			if("encrypt".equals(args[1])){
+				if(text.matches("^[a-zA-Z0-9 ]+"))
+						System.out.println(C.encoding(text,k));
+				else
+						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+				}
+			else if("decrypt".equals(args[1]))
+						C.decoding(text,k);
+			else if("bruteforce".equals(args[1])) 
+					if(text.matches("^[a-zA-Z0-9 ]+"))
+						  C.bruteforce(text);
+					 else {
+						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+			            System.exit(1);}
+	    }
+	    	
+	    if("RailFence".equals(args[0])){
+			if("encrypt".equals(args[1])){
+				if(text.matches("^[a-zA-Z ]+"))
+						R.encoding(text,k);
+				else 
+					System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+			}
+			else if("decrypt".equals(args[1]))
+				if(text.matches("^[a-zA-Z ]+")){
+					R.decoding(text,k);
+				}
+				else
+					{System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja");
+			        System.exit(1);}
+			if("Case".equals(args[0])){
+				if("lower".equals(args[1])){
+					if(text.matches("^[a-zA-Z ]+"))
+							Ca.lower(text);
+					else 
+						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+				}
+				else if("upper".equals(args[1]))
+					if(text.matches("^[a-zA-Z ]+")){
+						Ca.upper(text);
+					}
+					else
+						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+				    }
+			   else if("capitalize".equals(args[1]))
+				   if(text.matches("^[a-zA-Z ]+")){
+						Ca.capitalize(text);
+					}
+					else
+						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+				    }
+	           else if("alternating".equals(args[1]))
+			           if(text.matches("^[a-zA-Z ]+")){
+					       Ca.alternating(text);
+				     }
+				       else {
+					   System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+			         }
+	            else if("inverse".equals(args[1]))
+		             if(text.matches("^[a-zA-Z ]+")){
+				        Ca.inverse(text);
+			     }
+			         else {
+				        System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+				        System.exit(1);
+		    } 
+	    
+	    
+	    
+	    
+			
+	}
+	
+}
+	    
