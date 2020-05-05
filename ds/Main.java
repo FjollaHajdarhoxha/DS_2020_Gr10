ff773cc8320c552694fe955387c046af14095ba1
FjollaHajdarhoxha <44026066+FjollaHajdarhoxha@users.noreply.github.com>
2020-05-05 13:18:31 +0200

    Update Main.java

--- ds/Main.java
+++ ds/Main.java
@@ -11,23 +11,27 @@ public class Main  {
 	   Caesar C= new Caesar();
 	   RailFence R= new RailFence();
 	   Case Ca= new Case();	    
-	    if(args.length == 0 || args.length > 4 || args.length < 3){
+	   Create Cr= new Create();
+	   Delete D= new Delete();
+	    if(args.length == 0 || args.length > 4 || args.length<2){
 	    	
-	    	System.out.println("\n\t\tProgrami pranon deri në 4 argumente!\n");
-	    	 System.out.println("\t\t\tPërdorimi i Programit\n\n1.Për Kodin Caesar shtyp -> Caesar encrypt | decrypt <teksti> <celësi>\n" + "~Për BruteForce shtyp -> Caesar bruteforce <teksti>\n"+ "2.Për Kodin RailFence shtyp -> RailFence encrypt | decrypt <teksti> <celësi>\n" + "3.Për Kodin Case shtyp -> Case lower | upper | inverse | alternating | capitalize <teksti>");
+	    	System.out.println("\n\t\tProgrami pranon deri nÃ« 4 argumente!\n");
+	    	 System.out.println("\t\t\tPÃ«rdorimi i Programit\n\n1.PÃ«r Kodin Caesar shtyp -> Caesar encrypt | decrypt <teksti> <celÃ«si>\n" + "~PÃ«r BruteForce shtyp -> Caesar bruteforce <teksti>\n"+ "2.PÃ«r Kodin RailFence shtyp -> RailFence encrypt | decrypt <teksti> <celÃ«si>\n" + "3.PÃ«r Kodin Case shtyp -> Case lower | upper | inverse | alternating | capitalize <teksti>\n"+"4.Per te krijuar user shtyp -> create-user <emri>\n"+"5.Per te fshire user shtyp -> delete-user <emri>");
 				System.exit(1);
 			}
 	    
-	    String text = args[2];
-	    String key=args[3];
-	    int k=Integer.parseInt(key);
-	    
+
+	    if(args.length == 4|| args.length ==3){
+	    	 String text = args[2];
+	 	    String key=args[3];
+	 	    int k=Integer.parseInt(key);
 	    if("Caesar".equals(args[0])){
 			if("encrypt".equals(args[1])){
+   
 				if(text.matches("^[a-zA-Z0-9 ]+"))
 						System.out.println(C.encoding(text,k));
 				else
-						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
 				}
 			else if("decrypt".equals(args[1]))
 						C.decoding(text,k);
@@ -35,7 +39,7 @@ public class Main  {
 					if(text.matches("^[a-zA-Z0-9 ]+"))
 						  C.bruteforce(text);
 					 else {
-						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
 			            System.exit(1);}
 	    }
 	    	
@@ -44,57 +48,82 @@ public class Main  {
 				if(text.matches("^[a-zA-Z ]+"))
 						R.encoding(text,k);
 				else 
-					System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+					System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
 			}
 			else if("decrypt".equals(args[1]))
 				if(text.matches("^[a-zA-Z ]+")){
 					R.decoding(text,k);
 				}
 				else
-					{System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja");
+					{System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja");
 			        System.exit(1);}
+			
 			if("Case".equals(args[0])){
 				if("lower".equals(args[1])){
 					if(text.matches("^[a-zA-Z ]+"))
 							Ca.lower(text);
 					else 
-						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
 				}
 				else if("upper".equals(args[1]))
 					if(text.matches("^[a-zA-Z ]+")){
 						Ca.upper(text);
 					}
 					else
-						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");	
 				    }
 			   else if("capitalize".equals(args[1]))
 				   if(text.matches("^[a-zA-Z ]+")){
 						Ca.capitalize(text);
 					}
 					else
-						System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");	
 				    }
 	           else if("alternating".equals(args[1]))
 			           if(text.matches("^[a-zA-Z ]+")){
 					       Ca.alternating(text);
 				     }
 				       else {
-					   System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+					   System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");	
 			         }
 	            else if("inverse".equals(args[1]))
 		             if(text.matches("^[a-zA-Z ]+")){
 				        Ca.inverse(text);
 			     }
 			         else {
-				        System.out.println("\n\t\tTeksti duhet të përmbajë vetëm shkronja!\n");	
+				        System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");	
 				        System.exit(1);
-		    } 
-	    
-	    
-	    
+		         } }
+	    else {
+	            String text1=args[1];
+	            if("create-user".equals(args[0])){
+			    {
+				if(text1.matches("^[a-zA-Z0-9 ]+"))
+					try {
+						Cr.CreateUser(text1);
+					} catch (Exception e) {
+						
+						e.printStackTrace();
+					}
+				else
+						System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
+				}
+			    if("delete-user".equals(args[0])){
+				    {
+					if(text1.matches("^[a-zA-Z0-9 ]+"))
+						try {
+							D.DeleteUser(text1);
+						} catch (Exception e) {
+							
+							e.printStackTrace();
+						}
+					else
+							System.out.println("\n\t\tTeksti duhet tÃ« pÃ«rmbajÃ« vetÃ«m shkronja!\n");
+					}
 	    
 			
-	}
+	}}
 	
+	            }}}
 }
 	    
