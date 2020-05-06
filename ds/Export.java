9e9eea580233335eeacf87db69af88415439d9b1
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-05-06 22:30:31 +0200

    Update Export.java

--- ds/Export.java
+++ ds/Export.java
@@ -40,3 +40,35 @@ public class Export {
 	    }else
 	    	System.out.println("Gabim: Celesi publik '"+filename+"' nuk ekziston.");
 	}
+ public  void privateKey(String filename) throws Exception {
+	
+	   File file = new File("C:\\Users\\admin\\OneDrive\\Desktop\\keys\\"+filename+".xml");
+	   
+		
+	    if (file.exists()) {
+		DocumentBuilderFactory documentBuilderFactory=DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document=documentBuilder.parse("C:\\Users\\admin\\OneDrive\\Desktop\\keys\\"+filename+".xml");
+		
+		NodeList list=document.getElementsByTagName("RSAKeyValue");
+		for (int i=0; i<list.getLength();i++) {
+			Node node = list.item(i);
+			
+			if(node.getNodeType()==Node.ELEMENT_NODE) {
+				Element element=(Element) node;
+				
+				System.out.println("<RSAKeyValue>");
+				System.out.println("\n<Modulus>"+element.getElementsByTagName("Modulus").item(0).getTextContent()+"</Modulus>");
+				System.out.println("\n<Exponentent>"+element.getElementsByTagName("Exponent").item(0).getTextContent());
+				System.out.println("\n<P>"+element.getElementsByTagName("P").item(0).getTextContent()+"</P>");
+				System.out.println("\n<Q>"+element.getElementsByTagName("Q").item(0).getTextContent()+"</Q>");
+				System.out.println("\n<DP>"+element.getElementsByTagName("DP").item(0).getTextContent()+"</DP>");
+				System.out.println("\n<InverseQ>"+element.getElementsByTagName("InverseQ").item(0).getTextContent()+"</InverseQ>");
+				System.out.println("\n<D>"+element.getElementsByTagName("D").item(0).getTextContent()+"</D>");
+                System.out.println("\n</RSAKeyValue>");
+			}
+		}
+          }else
+          	System.out.println("Gabim: Celesi publik '"+filename+"' nuk ekziston.");
+	}
+}
